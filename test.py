1cf1543ce3e442e8315b07c978e31d4af3a83538
Martin Davies <mdavies@kellettschool.com>
2024-04-15 19:00:05 +0800

    modify tests

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ from robomasterpy import RoboMaster
 from random import randint
 
 rm = RoboMaster()
-r = randint(0,255)
-g = randint(0,255)
-b = randint(0,255)
-rm.setLEDs(r,g,b)
+r = randint(0, 255)
+g = randint(0, 255)
+b = randint(0, 255)
+rm.setLEDs(r, g, b)
 rm.turnLeft(90)
 rm.turnRight(90)
 rm.forward(0.5)
@@ -13,4 +13,5 @@ rm.left(0.5)
 rm.back(0.5)
 rm.right(0.5)
 rm.circle(0.25, 1)
-exit()
+# TODO: test how this actually moves, arc or straight line with directional change?
+rm.move(1, zRotation=90)
